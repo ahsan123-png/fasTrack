7c57f467383b092eecfc580ac64f7d1d45d1d6d9
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-11-19 01:16:47 +0500

    add

--- schedule/celery.py
+++ schedule/celery.py
@@ -1,11 +1,6 @@
 # schedule/celery.py
-
 from __future__ import absolute_import, unicode_literals
-
-# Import the main Celery application from fasTrack project
 from fasTrack.celery import app as celery_app
-
-# This file can be left empty or used for app-specific Celery configurations
 __all__ = ('celery_app',)
 
 

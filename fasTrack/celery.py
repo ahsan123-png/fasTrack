801ef3d68659bc80cbbdc43f84fcab7bef2a7cb6
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-11-19 02:15:35 +0500

    add celery

--- fasTrack/celery.py
+++ fasTrack/celery.py
@@ -1,8 +1,10 @@
-from celery import Celery
-from schedule import tasks as schedule_tasks  # Explicitly import tasks from 'schedule'
 import os
+from celery import Celery
+from schedule import task as schedule_tasks  # Explicitly import tasks from 'schedule'
 from celery.schedules import crontab 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fasTrack.settings')
+# print("DJANGO_SETTINGS_MODULE:", os.getenv('DJANGO_SETTINGS_MODULE'))
+if not os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'fasTrack.settings'
 app = Celery('fasTrack')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.broker_url = 'redis://13.60.13.74:6379/0'

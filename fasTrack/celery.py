3f78fa95d3169e05e3b7a169c99033db8ed4f640
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-11-19 01:33:41 +0500

    add celery url

--- fasTrack/celery.py
+++ fasTrack/celery.py
@@ -5,6 +5,7 @@ from celery.schedules import crontab
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fasTrack.settings')
 app = Celery('fasTrack')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = 'redis://13.60.13.74:6379/0'
 # Manually import the tasks
 app.tasks.register(schedule_tasks.schedule_expiry_notification)
 app.tasks.register(schedule_tasks.send_document_expiry_notification)

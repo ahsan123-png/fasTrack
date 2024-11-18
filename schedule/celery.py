ad45c5c737666eb1ab3dec09d86a99965be39957
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-11-18 21:54:56 +0500

    add feature

--- schedule/celery.py
+++ schedule/celery.py
@@ -5,7 +5,7 @@ import os
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fasTrack.settings')
-app = Celery('fasTrack')
+app = Celery('schedule')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 app.conf.beat_schedule = {

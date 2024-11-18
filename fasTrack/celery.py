05dba5e49df931abe0bfba026e15363f75deb98b
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-11-19 01:25:40 +0500

    add celery

--- fasTrack/celery.py
+++ fasTrack/celery.py
@@ -1,13 +1,17 @@
 from celery import Celery
-from celery.schedules import crontab  # Importing crontab for scheduling periodic tasks
+from schedule import tasks as schedule_tasks  # Explicitly import tasks from 'schedule'
 import os
+from celery.schedules import crontab 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fasTrack.settings')
 app = Celery('fasTrack')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.autodiscover_tasks('schedule')
+# Manually import the tasks
+app.tasks.register(schedule_tasks.schedule_expiry_notification)
+app.tasks.register(schedule_tasks.send_document_expiry_notification)
+app.tasks.register(schedule_tasks.notify_user)
 app.conf.beat_schedule = {
     'notify-users-every-day': {
         'task': 'schedule.tasks.notify_user',
-        'schedule': crontab(hour=8, minute=0), 
+        'schedule': crontab(hour=8, minute=0),
     },
 }
